858cdf08a8b13018d96cebac6c4a94812b2483d9
HAlsafwani <161020505+HAlsafwani@users.noreply.github.com>
2024-03-27 20:50:03 +0300

    Update index.js

--- addFunction/index.js
+++ addFunction/index.js
@@ -1,5 +1,13 @@
-exports.add = (req, res) => {
+const express = require('express');
+const app = express();
+
+app.get('/add', (req, res) => {
   const { X, Y } = req.query;
   const result = Number(X) + Number(Y);
   res.json({ X, Y, Result: result });
-};
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
