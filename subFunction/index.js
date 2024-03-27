5c6cfbd521d2b5ae3a75adb385586ee98e7101a3
HAlsafwani <161020505+HAlsafwani@users.noreply.github.com>
2024-03-27 20:54:33 +0300

    Add files via upload

---
+++ subFunction/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/sub', (req, res) => {
+  const { X, Y } = req.query;
+  const result = Number(X) - Number(Y);
+  res.json({ X, Y, Result: result });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
